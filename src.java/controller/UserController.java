fe291dae9b8a1f64ddfce36dfb6ce254769ddd45
YOLO <AshCamile@163.com>
2022-07-03 22:42:33 +0800

    test2...

--- src.java/controller/UserController.java
+++ src.java/controller/UserController.java
@@ -3,7 +3,7 @@ package controller;
 
 
 public class UserController {
-    private int id;
+    private int id = 10;
     public static void main(String[] args) {
         System.out.println();
     }
